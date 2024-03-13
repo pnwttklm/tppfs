a23d5b4fbf2886e84755540dfd5a46b818bda987
Poonyawatt  Klumnaim <poon@pnwttklm.com>
2024-03-13 18:53:27 +0700

    add pages

---
+++ src/app/dog/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <>
+      <h1>This is a dog page.</h1>
+    </>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+   <>
+      <h1>Hello it's poon.</h1>
+   </> 
+  );
+}
