7a4b7017ead6715ac6dd1189e5b64c2ec4658e18
Poonyawatt  Klumnaim <poon@pnwttklm.com>
2024-03-13 19:21:36 +0700

    Add tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+
+function Hello() {
+  const x = 1;
+  if (x === 1) {
+    return <h1>this is 1</h1>;
+  } else {
+    return <h1>This is as Hello</h1>;
+  }
+}
+
+
 export default function Home() {
+  const name = "P";
   return (
-   <>
-      <h1>Hello it is poon.</h1>
-   </> 
+    <>
+      <h1 className="text-[#00FF66] font-bold text-8xl italic mt-72">
+        Hello it is poon.
+      </h1>
+      <Hello />
+      {
+        name==="P"?"Jelwklekw":"wejkjolerkj"
+      }
+      <h1>{name}</h1>
+    </>
   );
 }
+
+
