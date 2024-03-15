d7fe3029567966aec40d293bd243013e705f862f
chisanupongkhamnung <chissanupong.kha@student.mahidol.edu>
2024-03-15 21:48:40 +0700

    created four path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <nav className={inter.className}>{children} this is nav bar</nav>
+      <nav className={inter.className}>{children}</nav>
     </html>
   );
 }
