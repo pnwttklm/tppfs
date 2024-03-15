a6b530e36ee21cb75de4a1be4215fd3dc909d26e
TataJarukit <jarukit.bun@student.mahidol.ac.th>
2024-03-15 20:18:55 +0700

    TATA path created
    All things seem fine but the fucking picture is not fucking show up I dont know why If you know how to fix it pls tell me. ChatGPT and Gemini is also cannot help me with this problem.

---
+++ src/app/TATA/page.tsx
@@ -0,0 +1,10 @@
+export default function God(){
+    return (
+        <div>
+            <img src="./ta.jpg" alt="This shit is always show up if you know how to fix it pls tell me"/>
+            <h1 className="mt-60 rounded-full border-4 border-sky-500 py-12 text-5xl text-center text-fuchsia-500">
+                Hey bro you just found god HERE.
+            </h1>
+        </div>
+    );
+}
