8b4ef95abbca4a0504436181ed6cf67ae65f162b
PunChayootpone <107537680+PunChayootpone@users.noreply.github.com>
2024-03-16 23:50:22 +0700

    Pun's Web page update
    Add gojo and my name

---
+++ src/app/Pun/page.tsx
@@ -0,0 +1,9 @@
+export default
+function Pun(){
+    return( 
+    <div>
+        Chayootpone Kaweetraiphop
+  <img src="/gojo_saturo_xavier_skin_mlbb_8k_png_by_newjer53_dfw5shj-pre.png" alt="" />
+        </div>)
+   
+}
