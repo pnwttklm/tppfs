0096200de16c6159464025f4f0e9446c0185ab7b
Chayootpone039 <150504788+Chayootpone039@users.noreply.github.com>
2024-03-17 21:59:20 +0700

    Test New PC
    New PC testing

--- src/app/Pun/page.tsx
+++ src/app/Pun/page.tsx
@@ -3,6 +3,7 @@ function Pun(){
     return( 
     <div>
         Chayootpone Kaweetraiphop
+        Pun Pun Pun
   <img src="/gojo_saturo_xavier_skin_mlbb_8k_png_by_newjer53_dfw5shj-pre.png" alt="" />
         </div>)
    
