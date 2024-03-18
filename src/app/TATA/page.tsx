17aebad489c3f9e8d1a6b4749d2c8a9d30e39fa3
Pakaporn-Chokpiroon <pakaporn.cho@student.mahidol.ac.th>
2024-03-18 20:09:09 +0700

    TATA changes

--- src/app/TATA/page.tsx
+++ src/app/TATA/page.tsx
@@ -1,10 +1,42 @@
+function Butto(){
+    return (
+        <div>
+            <button>{ool.name}</button>
+        </div>
+    );
+}
+
+const ool = {
+    name: 'bbab',
+    id: 123
+}
+
+const ttt = [
+    {
+        name: 'dsmnflkm',
+        id: 125453
+    },
+    {
+        name: 'bbdddddab',
+        id: 9999
+    }
+]
+
 export default function God(){
     return (
         <div>
+            {ttt.map((name,id) => (
+                <>
+                <h1>{id}</h1>
+                <h1>{name.id}</h1>
+                </>
+            ))
+            }
             <img src="./ta.jpg" alt="This shit is always show up if you know how to fix it pls tell me"/>
             <h1 className="mt-60 rounded-full border-4 border-sky-500 py-12 text-5xl text-center text-fuchsia-500">
                 Hey bro you just found god HERE.
             </h1>
+            <Butto />
         </div>
     );
 }
