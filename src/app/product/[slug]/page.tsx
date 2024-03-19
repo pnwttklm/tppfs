2769ac8d359c05ecfcc81f7c3c0973cee4455163
Chayootpone039 <150504788+Chayootpone039@users.noreply.github.com>
2024-03-20 00:02:02 +0700

    fix the error of product slug

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -53,7 +53,7 @@ const Page = () => {
   );
 };
 
-const InputField = ({ label }) => (
+const InputField = ({ label }: { label: string }) => (
   <div className="mb-2">
     <label className="block text-gray-700 text-sm font-bold mb-1">{label}:</label>
     <input type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
