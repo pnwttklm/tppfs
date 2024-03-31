1aef5b763addf98070d652eb00833f328177625d
Poonyawatt  Klumnaim <poon@pnwttklm.com>
2024-03-31 13:32:57 +0700

    finished login function

--- server/app.js
+++ server/app.js
@@ -1,54 +1,68 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const express = require('express');
+const express = require("express");
+const cors = require("cors"); // Import cors middleware
 const app = express();
 const port = process.env.PORT || 3030;
-const path = require('path');
+const path = require("path");
 const router = express.Router();
+app.use(cors()); // Use cors middleware
 app.use(router);
-router.use(express.urlencoded({extended: true}));
-router.use(express.json());  
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
 
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 var connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
-connection.connect(function(err) {
-    if (err) {
-        return console.error('error: ' + err.message);
+connection.connect(function (err) {
+  if (err) {
+    return console.error("error: " + err.message);
+  }
+  console.log("Connected to the MySQL server.");
+});
+
+router.get("/", (req, res) => {
+  res.send("hello world, the server is running!");
+});
+
+router.get("/api/v1/car", (req, res) => {
+  connection.query(`SELECT * FROM car`, function (err, results) {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+router.post("/api/v1/login", cors(), (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  connection.query(
+    `SELECT COUNT(*) AS count FROM account WHERE username = ? AND password = ?`,
+    [username, password],
+    function (err, results) {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Internal Server Error");
+      }
+      console.log(String(username));
+      console.log(password);
+      if (results[0].count === 1) {
+        res.json({ pass: true });
+      }else{
+        res.json({ pass: false });
+      }
     }
-    console.log('Connected to the MySQL server.');
-}   );
+  );
+});
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/html/search.html'));
-} );
+router.use(function (req, res, next) {
+  res.status(404).send("404: Page not Found");
+});
 
-router.post('/form-submit', (req, res) => {
-    if(req.body.search_choice === 'ID') {
-        connection.query(`SELECT * FROM professor WHERE EMP_NUM = ${req.body.search_value}`, function(err, results) {
-            if(err) throw err;
-            
-            if (results.length === 0) {
-                return res.sendFile(path.join(__dirname + '/html/notfound.html'));
-            }
-            res.send(results);
-        });
-    }else if(req.body.search_choice === 'name') {
-        connection.query(`SELECT * FROM professor WHERE CONCAT(EMP_FNAME, EMP_LNAME) LIKE "%${req.body.search_value}%"`, function(err, results) {
-            if(err) throw err;
-            if (results.length === 0) {
-                return res.sendFile(path.join(__dirname + '/html/notfound.html'));
-            }
-            res.send(results);
-        });
-    }
-}   );
-
-app.listen(port, function() {
-    console.log('Server is running on port: ' + port);
+app.listen(port, function () {
+  console.log("Server is running on port: " + port);
 });
--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import {
   Alert,
@@ -12,53 +12,115 @@ import {
   InputRightElement,
   Divider,
   Button,
-} from '@chakra-ui/react'
-import {BsPerson, BsEnvelope, BsLock, BsTelephone, BsEyeFill, BsEyeSlashFill} from "react-icons/bs";
+} from "@chakra-ui/react";
+import {
+  BsPerson,
+  BsEnvelope,
+  BsLock,
+  BsTelephone,
+  BsEyeFill,
+  BsEyeSlashFill,
+} from "react-icons/bs";
 import Image from "next/image";
 // import Users from '../../data/users.js';
 
 export default function LoginPage() {
-  const [show, setShow] = React.useState(false)
-  const [ID, setID] = useState('')
-  const [password, setPassword] = useState('')
-  const handleID = (e:any) => setID(e.target.value)
-  const handlePass = (v:any) => setPassword(v.target.value)
-  const handleClick = () => setShow(!show)
+  const [show, setShow] = React.useState(false);
+  const [ID, setID] = useState("");
+  const [password, setPassword] = useState("");
+  const handleID = (e: any) => setID(e.target.value);
+  const handlePass = (v: any) => setPassword(v.target.value);
+  const handleClick = () => setShow(!show);
+  const axios = require("axios");
+  function DefaultPage() {
+    const axios = require("axios");
+    let data = JSON.stringify({
+      username: String(ID),
+      password: String(password),
+    });
 
-  function DefaultPage(){
-    return location.href = '/admin';
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "http://localhost:3030/api/v1/login",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios
+      .request(config)
+      .then((response:any) => {
+        if(response.data.pass===true){
+          window.location.href = "/admin";
+      }
+      else{
+        alert("Invalid Username or Password");
+      }
+    })
+      .catch((error:any) => {
+        console.log(error);
+      });
   }
-  
-  
-    return (
-      <div className='bg-[#F5F5F5] h-screen'>
-            <Center className="h-full">
-                <Center className="bg-[#FFFFFF] mx-6 rounded-3xl flex flex-col p-12 mt-8 w-fit shadow-2xl">
-                <img src="/logo.svg" width={100} height={100}></img><br></br>
-                <h1 className="text-3xl font-extrabold mb-6">Login</h1>
-                <div className="text-[#1C3B5A] mt-6">
-                        <InputGroup className="mt-8 bg-[#FFFFFF]" size='lg'>
-                            <InputLeftElement pointerEvents='none'>
-                            <BsPerson color='#807A7A' />
-                            </InputLeftElement>
-                            <Input placeholder='Username' value={ID} type='text' onChange={handleID} rounded={'2xl'}/>
-                        </InputGroup>
-                        <InputGroup className="mt-8 bg-[#FFFFFF]" size='lg'>
-                            <InputLeftElement pointerEvents='none'>
-                                <BsLock color='#807A7A' />
-                            </InputLeftElement>
-                            <Input pr='4.5rem' rounded={'2xl'} value={password} onChange={handlePass}
-                            type={show ? 'text' : 'password'} placeholder='Password'/>
-                            <InputRightElement width='4.5rem'>
-                                <Button h='1.75rem' size='sm' onClick={handleClick}>
-                                {show ? <BsEyeSlashFill color='##1C3B5A' /> : <BsEyeFill color='##1C3B5A' /> }
-                                </Button>
-                            </InputRightElement>
-                        </InputGroup>
-                </div>
-                    <Button onClick={() => DefaultPage()} className="rounded-full border border-[#3E0070] p-2  text-[#FFFFFF] bg-[#3E0070] hover:bg-[#FFFFFF] hover:text-[#3E0070]" mt={'16'} px={'6'} py={'3'} color={'white'} backgroundColor={'#3E0070'} rounded={'full'}>Login</Button>
-                </Center>
-            </Center>
-      </div>
-    );
-  }
+
+  return (
+    <div className="bg-[#F5F5F5] h-screen">
+      <Center className="h-full">
+        <Center className="bg-[#FFFFFF] mx-6 rounded-3xl flex flex-col p-12 mt-8 w-fit shadow-2xl">
+          <img src="/logo.svg" width={100} height={100}></img>
+          <br></br>
+          <h1 className="text-3xl font-extrabold mb-6">Login</h1>
+          <div className="text-[#1C3B5A] mt-6">
+            <InputGroup className="mt-8 bg-[#FFFFFF]" size="lg">
+              <InputLeftElement pointerEvents="none">
+                <BsPerson color="#807A7A" />
+              </InputLeftElement>
+              <Input
+                placeholder="Username"
+                value={ID}
+                type="text"
+                onChange={handleID}
+                rounded={"2xl"}
+              />
+            </InputGroup>
+            <InputGroup className="mt-8 bg-[#FFFFFF]" size="lg">
+              <InputLeftElement pointerEvents="none">
+                <BsLock color="#807A7A" />
+              </InputLeftElement>
+              <Input
+                pr="4.5rem"
+                rounded={"2xl"}
+                value={password}
+                onChange={handlePass}
+                type={show ? "text" : "password"}
+                placeholder="Password"
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleClick}>
+                  {show ? (
+                    <BsEyeSlashFill color="##1C3B5A" />
+                  ) : (
+                    <BsEyeFill color="##1C3B5A" />
+                  )}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </div>
+          <Button
+            onClick={() => DefaultPage()}
+            className="rounded-full border border-[#3E0070] p-2  text-[#FFFFFF] bg-[#3E0070] hover:bg-[#FFFFFF] hover:text-[#3E0070]"
+            mt={"16"}
+            px={"6"}
+            py={"3"}
+            color={"white"}
+            backgroundColor={"#3E0070"}
+            rounded={"full"}
+          >
+            Login
+          </Button>
+        </Center>
+      </Center>
+    </div>
+  );
+}
