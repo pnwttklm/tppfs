10f04f99355d360caa53564c0ddeadb19d4dc749
TataJarukit <jarukit.bun@student.mahidol.ac.th>
2024-04-21 11:55:57 +0700

    Add our-team pictures

--- web/src/app/our-team/page.tsx
+++ web/src/app/our-team/page.tsx
@@ -49,11 +49,11 @@ function Allofus() {
       <div className="border border-gray bg-white p-6 rounded-3xl shadow-lg drop-shadow-lg md:w-auto mx-10">
         <Center>
           <Stack spacing={8} direction='row'>
-            <Acard name="TATA" position="BackEnd"/>
-            <Acard name="Pun" position="FrontEnd"/>
-            <Acard name="Poon" position="Leader"/>
-            <Acard name="Four" position="FrontEnd"/>
-            <Acard name="Shuiiy" position="FrontEnd"/>
+            <Acard name="TATA" position="BackEnd" pic = "./TATA.jpg"/>
+            <Acard name="Pun" position="FrontEnd" pic = "./pun.jpg"/>
+            <Acard name="Poon" position="Leader" pic = "./poon.jpg"/>
+            <Acard name="Four" position="FrontEnd" pic = "./4.jpg"/>
+            <Acard name="Shuiiy" position="FrontEnd" pic = "./Shuii.jpg"/>
           </Stack>
         </Center>
       </div>
@@ -64,16 +64,17 @@ function Allofus() {
 interface AcardProps {
   name: string;
   position: string;
+  pic : string;
 }
 
-function Acard({ name, position }: AcardProps){
+function Acard({ name, position, pic}: AcardProps){
   return(
     <>
     <Card maxW='sm'>
       <CardBody>
         <div className="m-1">
           <Image 
-            src='./ta.jpg'
+            src={pic}
             alt='HA'
             boxSize='280px'
             borderRadius='full'
